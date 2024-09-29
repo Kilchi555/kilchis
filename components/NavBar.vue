95344a5f0e4514f366839804947cea45edcfb0e9
Kilchi555 <pascal_kilchenmann@icloud.com>
2024-09-29 21:35:03 +0200

    Homepage

---
+++ components/NavBar.vue
@@ -0,0 +1,5 @@
+<template>
+      <header class="sticky top-0 z-50 flex justify-between items-center space-x-1 border-b bg-white p-4 shadow-md">
+        <a class="text-4xl font-monomono text-green-400" href="">Kilchis<span class="text-gray-600">.ch</span></a>
+      </header>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+      <NavBar />
+      <Hero />
+    </div>
+  </template>
